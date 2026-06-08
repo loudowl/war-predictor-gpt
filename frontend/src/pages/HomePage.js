e7f7eadabbcee7940ae955c1990e3735ba9465f4
Chris <loudowl@mac.com>
2026-06-08 11:35:02 -0700

    feat: initial implementation

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Military Activity Reporting App</h1>
+    </div>
+  );
+};
+
+export default HomePage;
